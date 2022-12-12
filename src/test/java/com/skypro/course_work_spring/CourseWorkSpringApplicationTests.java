59bbd78847a81cf0d68270102dbbba08d60b876f
gubina <gubinamarina1998@gmail.com>
2022-12-12 11:13:11 +0300

    Initial commit

---
+++ src/test/java/com/skypro/course_work_spring/CourseWorkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.course_work_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
